4dfc592504b698d5b4db885681364a10ad9147a3
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2020-12-25 21:38:12 -0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,50 @@
 import { render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 
 
 function App() {
 
-  const users = [
-    {name:"Juan Cruz" , email:"juancb@bidete.com", page:"juancb.com"},
-    {name:"Teo" , email:"tg27@teogol.com", page:"fcbajunior.com"},
-    {name:"Santino" , email:"santinogb@gmail.com", page:"santinog.com"},
-    {name:"Nacho" , email:"nacho@wetzel.com", page:"igna.com"},
-  ]
+  const [users, setUsers] = useState([])
 
-  const renderInfo = () => {
-    return users.map((user) => {
-      return(
-        <tr>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>{user.page}</td>
-      </tr>
-      )
-    })
-  }
+  useEffect(() => {
+    const resp = axios.get("https://jsonplaceholder.typicode.com/users")
+      .then(resp => setUsers(resp.data))
+  },[])
 
-  const ponerFilas = () => [ 
-    <tr>
-      <td>Juan </td>
-      <td>admin@email.com</td>
-      <td>jcbidegain.com</td>
-    </tr>,
-      <tr>
-        <td>Santino </td>
-        <td>santino.g@email.com</td>
-        <td>santinobidgain.com</td>
-      </tr>
-   ]
+  // const users = [
+  //   {name:"Juan Cruz" , email:"juancb@bidete.com", page:"juancb.com"},
+  //   {name:"Teo" , email:"tg27@teogol.com", page:"fcbajunior.com"},
+  //   {name:"Santino" , email:"santinogb@gmail.com", page:"santinog.com"},
+  //   {name:"Nacho" , email:"nacho@wetzel.com", page:"igna.com"},
+  // ]
+
+  // const renderInfo = () => {
+  //   return users.map((user) => {
+  //     return(
+  //       <tr>
+  //         <td>{user.name}</td>
+  //         <td>{user.email}</td>
+  //         <td>{user.page}</td>
+  //     </tr>
+  //     )
+  //   })
+  // }
+
+  // const ponerFilas = () => [ 
+  //   <tr>
+  //     <td>Juan </td>
+  //     <td>admin@email.com</td>
+  //     <td>jcbidegain.com</td>
+  //   </tr>,
+  //     <tr>
+  //       <td>Santino </td>
+  //       <td>santino.g@email.com</td>
+  //       <td>santinobidgain.com</td>
+  //     </tr>
+  //  ]
 
 
 
@@ -51,7 +59,15 @@ function App() {
         </tr>
       </thead>
       <tbody>
-        {renderInfo()}
+        {users.map(user => {
+          return(
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td><a href="">{user.website}</a></td>
+            </tr>
+          )
+        })}
       </tbody>
     </table>
 
